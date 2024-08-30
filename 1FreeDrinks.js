7ea29e7e94e4532b260198e161a7eef69945f085
AlaminIbneFaruk <alaminibnefarukayon@gail.com>
2024-08-30 10:45:21 +0600

    first commit

---
+++ 1FreeDrinks.js
@@ -0,0 +1,12 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+var burgerPrice=750;
+if(burgerPrice>500){
+  console.log("free coke");
+}else{
+  console.log("coke is 30tk");
+}
